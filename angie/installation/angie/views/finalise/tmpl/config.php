daac54b2785e748c3e5ecb78d5f82f719046616d
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2018-11-03 23:52:12 +0200

    gh-50 Convert to FEF
    Joomla installer, finalization page

--- angie/installation/angie/views/finalise/tmpl/config.php
+++ angie/installation/angie/views/finalise/tmpl/config.php
@@ -9,23 +9,28 @@
 defined('_AKEEBA') or die();
 
 ?>
-<div class="alert alert-warning">
+<div class="akeeba-block--warning">
 	<?php echo AText::_('FINALISE_LBL_DONTFORGETCONFIG'); ?>
 </div>
 
-<h3>
-	<?php echo AText::_('FINALISE_HEADER_CONFIGURATION'); ?>
-</h3>
-<p>
-	<?php echo AText::_('FINALISE_LBL_CONFIGINTRO'); ?>
-</p>
-<pre class="scrollmore">
-<?php echo htmlentities($this->configuration) ?>
-</pre>
-<p>
-	<?php echo AText::_('FINALISE_LBL_CONFIGOUTRO'); ?>
-</p>
+<div class="akeeba-panel--info">
+	<header class="akeeba-block-header">
+		<h3>
+			<?php echo AText::_('FINALISE_HEADER_CONFIGURATION'); ?>
+		</h3>
+	</header>
+	<p>
+		<?php echo AText::_('FINALISE_LBL_CONFIGINTRO'); ?>
+	</p>
+	<pre class="scrollmore"><?php echo htmlentities($this->configuration) ?></pre>
+	<p>
+		<?php echo AText::_('FINALISE_LBL_CONFIGOUTRO'); ?>
+	</p>
 
-<h3>
-	<?php echo AText::_('FINALISE_HEADER_AFTERCONFIGURATION'); ?>
-</h3>
+</div>
+<div class="akeeba-panel--orange">
+	<header class="akeeba-block-header">
+		<h3>
+			<?php echo AText::_('FINALISE_HEADER_AFTERCONFIGURATION'); ?>
+		</h3>
+	</header>
