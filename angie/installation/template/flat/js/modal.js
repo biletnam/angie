91734ede7d410f113ba1504c93603277a385ce90
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2019-01-01 13:54:43 +0200

    Updated Copyright headers. Happy New Year!

--- angie/installation/template/flat/js/modal.js
+++ angie/installation/template/flat/js/modal.js
@@ -1,7 +1,9 @@
-/**
- * @package     Solo
- * @copyright Copyright (c)2014-2018 Nicholas K. Dionysopoulos / Akeeba Ltd
- * @license     GNU GPL version 3 or later
+/*
+ * ANGIE - The site restoration script for backup archives created by Akeeba Backup and Akeeba Solo
+ *
+ * @package   angie
+ * @copyright Copyright (c)2009-2019 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU/GPL v3 or later
  */
 
 // Object initialisation
--- build/templates/version.php
+++ build/templates/version.php
@@ -1,8 +1,10 @@
 <?php
 /**
- * @package   AkeebaBackup
- * @copyright Copyright (c)2006-2018 Nicholas K. Dionysopoulos / Akeeba Ltd
- * @license   GNU General Public License version 3, or later
+ * ANGIE - The site restoration script for backup archives created by Akeeba Backup and Akeeba Solo
+ *
+ * @package   angie
+ * @copyright Copyright (c)2009-2019 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU/GPL v3 or later
  */
 
 // Protect from unauthorized access
